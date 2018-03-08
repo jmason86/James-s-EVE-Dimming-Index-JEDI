fae7bedf20edd11075195d04a2536bb270945cdc
James Paul Mason <jmason86@gmail.com>
2018-03-08 13:58:28 -0500

    Added max_second_shift parameter

--- light_curve_peak_match_subtract.py
+++ light_curve_peak_match_subtract.py
@@ -13,6 +13,7 @@ __contact__ = 'jmason86@gmail.com'
 
 
 def light_curve_peak_match_subtract(light_curve_to_subtract_from_df, light_curve_to_subtract_with_df, estimated_time_of_peak,
+                                    max_seconds_shift=1800,
                                     plot_path_filename=None, verbose=False, logger=None):
     """Align the peak of a second light curve to the first, scale its magnitude to match, and subtract it off.
 
@@ -22,6 +23,7 @@ def light_curve_peak_match_subtract(light_curve_to_subtract_from_df, light_curve
         estimated_time_of_peak [metatime]: The estimated time that the peak should occur. This could come from, e.g., GOES/XRS.
 
     Optional Inputs:
+        max_seconds_shift [int]:  The maximum allowed time shift in seconds to get the peaks to match.
         plot_path_filename [str]: Set to a path and filename in order to save the summary plot to disk.
                                   Default is None, meaning the plot will not be saved to disk.
         verbose [bool]:           Set to log the processing messages to disk and console. Default is False.
@@ -98,26 +100,35 @@ def light_curve_peak_match_subtract(light_curve_to_subtract_from_df, light_curve
     seconds_shift = (light_curve_to_subtract_from_df.index[peak_index_from] -
                      light_curve_to_subtract_with_df.index[peak_index_with]).total_seconds()
 
+    # Fail if seconds_shift > max_seconds_shift
+    isTimeShiftValid = True
+    if abs(seconds_shift) > max_seconds_shift:
+        if verbose:
+            logger.warning("Cannot do peak match. Time shift of {0} seconds is greater than max allowed shift of {1} seconds.".format(seconds_shift, max_seconds_shift))
+        isTimeShiftValid = False
+
     # Shift the subtract_with light curve in time to align its peak to the subtract_from light curve
-    if verbose:
-        logger.info("Shifting and scaling the light curve to subtract with.")
-    shifted_with = light_curve_to_subtract_with_df.shift(index_shift)
+    if isTimeShiftValid:
+        if verbose:
+            logger.info("Shifting and scaling the light curve to subtract with.")
+        shifted_with = light_curve_to_subtract_with_df.shift(index_shift)
 
-    # Scale the subtract_with light curve peak irradiance to match the subtract_from light curve peak irradiance
-    scale_factor = (detrend_from.values[peak_index_from] / shifted_with.values[peak_index_with + index_shift])[0]
-    shifted_scaled_with = shifted_with * scale_factor
-    light_curve_corrected_df = light_curve_to_subtract_from_df - shifted_scaled_with
+        # Scale the subtract_with light curve peak irradiance to match the subtract_from light curve peak irradiance
+        scale_factor = (detrend_from.values[peak_index_from] / shifted_with.values[peak_index_with + index_shift])[0]
+        shifted_scaled_with = shifted_with * scale_factor
+        light_curve_corrected_df = light_curve_to_subtract_from_df - shifted_scaled_with
 
-    if verbose:
-        if light_curve_corrected_df.isnull().values.sum() > 1:
-            logger.warning("%s points were shifted to become NaN." % light_curve_corrected_df.isnull().values.sum())
-        logger.info("Light curve peak matching and subtraction complete.")
+        if verbose:
+            if light_curve_corrected_df.isnull().values.sum() > 1:
+                logger.warning("%s points were shifted to become NaN." % light_curve_corrected_df.isnull().values.sum())
+            logger.info("Light curve peak matching and subtraction complete.")
 
     if plot_path_filename:
         from jpm_number_printing import latex_float
-        seconds_shift_string = '+' if seconds_shift >= 0 else '-'
+        seconds_shift_string = '+' if seconds_shift >= 0 else ''
         seconds_shift_string += str(int(seconds_shift))
-        scale_factor_string = latex_float(scale_factor)
+        if isTimeShiftValid:
+            scale_factor_string = latex_float(scale_factor)
 
         plt.style.use('jpm-transparent-light')
         from matplotlib import dates
@@ -126,17 +137,27 @@ def light_curve_peak_match_subtract(light_curve_to_subtract_from_df, light_curve
         fig, ax = plt.subplots()
         plt.plot(light_curve_to_subtract_from_df.index.values, light_curve_to_subtract_from_df.values, c='limegreen')
         plt.tick_params(axis='x', which='minor', labelbottom='off')
-        shifted_scaled_with.plot(c='tomato', label='subtract with', ax=ax)
-        light_curve_corrected_df.plot(c='darkgrey', label='result', ax=ax)
-        plt.scatter(light_curve_to_subtract_from_df.index[peak_index_from], light_curve_to_subtract_from_df.values[peak_index_from], c='black')
-        plt.scatter(shifted_scaled_with.index[peak_index_with + index_shift], shifted_scaled_with.values[peak_index_with + index_shift], c='black')
-        ax.legend(['subtract from', 'subtract with', 'result'], loc='best')
         plt.xlabel(estimated_time_of_peak)
         plt.ylabel('Irradiance [%]')
-        plt.title('I: $\\times$' + scale_factor_string + ', t: ' + seconds_shift_string + ' s', color='tomato')
         fmtr = dates.DateFormatter("%H:%M:%S")
         ax.xaxis.set_major_formatter(fmtr)
         ax.xaxis.set_major_locator(dates.HourLocator())
+
+        if isTimeShiftValid:
+            plt.title('I: $\\times$' + scale_factor_string + ', t: ' + seconds_shift_string + ' s', color='tomato')
+            shifted_scaled_with.plot(c='tomato', label='subtract with', ax=ax)
+            light_curve_corrected_df.plot(c='darkgrey', label='result', ax=ax)
+        else:
+            plt.title('t: ' + seconds_shift_string + ' s > max allowed {0} s'.format(max_seconds_shift), color='tomato')
+            plt.plot(light_curve_to_subtract_with_df.index.values, light_curve_to_subtract_with_df.values, c='tomato')
+        plt.scatter(light_curve_to_subtract_from_df.index[peak_index_from], light_curve_to_subtract_from_df.values[peak_index_from], c='black')
+
+        if isTimeShiftValid:
+            plt.scatter(shifted_scaled_with.index[peak_index_with + index_shift], shifted_scaled_with.values[peak_index_with + index_shift], c='black')
+            ax.legend(['subtract from', 'subtract with', 'result'], loc='best')
+        else:
+            plt.scatter(light_curve_to_subtract_with_df.index[peak_index_with], light_curve_to_subtract_with_df.values[peak_index_with], c='black')
+            ax.legend(['subtract from', 'subtract with'], loc='best')
 
         path = os.path.dirname(plot_path_filename)
         if not os.path.exists(path):
@@ -146,4 +167,7 @@ def light_curve_peak_match_subtract(light_curve_to_subtract_from_df, light_curve
         if verbose:
             logger.info("Summary plot saved to %s" % plot_path_filename)
 
-    return light_curve_corrected_df, seconds_shift, scale_factor
+    if isTimeShiftValid:
+        return light_curve_corrected_df, seconds_shift, scale_factor
+    else:
+        return np.nan, seconds_shift, np.nan
