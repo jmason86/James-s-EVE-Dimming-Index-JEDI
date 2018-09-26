132bbc7f4e09658d426d6581e5e400bfbd284104
James Paul Mason <jmason86@gmail.com>
2018-09-26 14:39:27 -0400

    Added light curve that can be created: preflare

--- tests/make_light_curve.py
+++ tests/make_light_curve.py
@@ -1,5 +1,7 @@
 import jedi_config
 import pandas as pd
+from astropy.time import Time
+import astropy.units as u
 
 jedi_config.init()  # Takes about 60 seconds
 
@@ -12,3 +14,11 @@ def make_light_curve():
 
 def normalized_irradiance_in_percent_units(light_curve):
     return pd.DataFrame((light_curve['irradiance'] - light_curve['irradiance'].iloc[0]) / light_curve['irradiance'].iloc[0] * 100.0)
+
+
+def make_preflare_light_curve():
+    flare_peak_time = '2010-08-07 18:24:00'
+    preflare_start_time = (Time(flare_peak_time) - (jedi_config.threshold_time_prior_flare_minutes * u.minute)).iso
+    light_curve = pd.DataFrame(jedi_config.eve_lines.loc[preflare_start_time:flare_peak_time, '17.1'])
+    light_curve.columns = ['irradiance']
+    return light_curve
