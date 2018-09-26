b22585c8f4cd20ddc9f16c33561380ee5c3e791f
James Paul Mason <jmason86@gmail.com>
2018-09-26 14:39:00 -0400

    Use numpy assert_approx_equal instead of string comparisons

--- tests/test_determine_dimming_depth.py
+++ tests/test_determine_dimming_depth.py
@@ -1,6 +1,7 @@
 from make_light_curve import make_light_curve, normalized_irradiance_in_percent_units
 from determine_dimming_depth import determine_dimming_depth
 import numpy as np
+from numpy.testing import assert_approx_equal
 import pandas as pd
 
 
@@ -9,9 +10,9 @@ def test_depth():
     assert no_points_below_zero_returns_nan(light_curve)
 
     light_curve = normalized_irradiance_in_percent_units(light_curve)
-    assert nominal_case_returns_expected_values(light_curve)
-    assert early_cutoff_works(light_curve)
-    assert late_cutoff_works(light_curve)
+    nominal_case_returns_expected_values(light_curve)
+    early_cutoff_works(light_curve)
+    late_cutoff_works(light_curve)
 
 
 def no_points_below_zero_returns_nan(light_curve):
@@ -22,8 +23,8 @@ def no_points_below_zero_returns_nan(light_curve):
 
 def nominal_case_returns_expected_values(light_curve):
     depth, time = determine_dimming_depth(light_curve.copy(), smooth_points=50)
-    if ('{0:.2f}'.format(depth) == '1.66') and (time == pd.Timestamp('2010-08-07 19:36:11')):
-        return True
+    assert_approx_equal(depth, 1.66, significant=3)
+    assert time == pd.Timestamp('2010-08-07 19:36:11')
 
 
 def early_cutoff_works(light_curve):
@@ -31,8 +32,8 @@ def early_cutoff_works(light_curve):
     depth, time = determine_dimming_depth(light_curve.copy(),
                                           smooth_points=50,
                                           earliest_allowed_time=time_early)
-    if ('{0:.2f}'.format(depth) == '1.79') and (time == pd.Timestamp('2010-08-07 20:10:11')):
-        return True
+    assert_approx_equal(depth, 1.79, significant=3)
+    assert time == pd.Timestamp('2010-08-07 20:10:11')
 
 
 def late_cutoff_works(light_curve):
@@ -40,5 +41,5 @@ def late_cutoff_works(light_curve):
     depth, time = determine_dimming_depth(light_curve.copy(),
                                           smooth_points=50,
                                           latest_allowed_time=time_late)
-    if ('{0:.2f}'.format(depth) == '0.84') and (time == pd.Timestamp('2010-08-07 19:15:11')):
-        return True
+    assert_approx_equal(depth, 0.841, significant=3)
+    assert time == pd.Timestamp('2010-08-07 19:15:11')
